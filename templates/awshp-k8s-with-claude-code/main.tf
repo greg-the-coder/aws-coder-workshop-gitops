b5d0cadc11481c7d4d9349c0f8955b86d8c85ec6
greg-the-coder <greg@coder.com>
2026-04-15 16:30:16 +0000

    syncing intermediate updates

--- templates/awshp-k8s-with-claude-code/main.tf
+++ templates/awshp-k8s-with-claude-code/main.tf
@@ -17,7 +17,7 @@ terraform {
 
 variable "namespace" {
   type        = string
-  description = "The Kubernetes namespace to create workspaces in (must exist prior to creating workspaces). If the Coder host is itself running as a Pod on the same Kubernetes cluster as you are deploying workspaces to, set this to the same namespace."
+  description = "The Kubernetes namespace to create workspaces in (must exist prior to creating workspaces)."
   default     = "coder"
 }
 
